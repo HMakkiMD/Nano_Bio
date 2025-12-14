6f5577d46dd8ec46115981ca9144f93348cba12d
Hesam Makki <107540794+HMakkiMD@users.noreply.github.com>
2025-12-14 19:32:54 +0000

    This repository contains all input files, simulation workflows, and representative output structures used in a multiscale molecular dynamics (MD) study of self-assembled amphiphilic nanoparticles (NPs) and their interactions with biomimetic lipid membranes.

---
+++ reverse-mapping/reverse-mapping/G1HH-mapping/run_min.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH --job-name G1HHOH
+#SBATCH -p gputroisi
+#SBATCH -N 1
+#SBATCH -n 24
+#SBATCH --gres gpu:1
+#SBATCH --time 5-00:00:00
+#SBATCH --mail-type END
+#SBATCH --mail-user h.makki@liverpool.ac.uk
+
+#module load apps/gromacs_cuda/2024.1
+#module load apps/anaconda3/2022.10
+#source activate myenv
+
+#python3 aa_itp_gen.py
+bash initram-v5-gmx.sh -f product_modified.gro -o AA.gro -from martini -to charmm36 -p topol.top -np 18
